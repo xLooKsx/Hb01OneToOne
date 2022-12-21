25328f6d015327cc51d695672dc5ed1f1d30dcb8
Lucas Oliveira <lu.oliveira.silva98@gmail.com>
2022-12-21 15:28:17 +0000

    New examples added

--- src/br/pessoal/hibernate/demo/DeleteInstructorDetailDemo.java
+++ src/br/pessoal/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -6,10 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.Arrays;
-import java.util.Objects;
-
-public class GetInstructorDetailDemo {
+public class DeleteInstructorDetailDemo {
     public static void main(String[] args) {
 
         SessionFactory sessionFactory = new Configuration()
@@ -24,12 +21,18 @@ public class GetInstructorDetailDemo {
 
             session.beginTransaction();
 
-            int theId = 123;
+            int theId = 3;
 
             InstructorDetail instructorDetail = session.get(InstructorDetail.class, theId);
 
             System.out.println("The instructor detail: " + instructorDetail);
             System.out.println("The associated instructor: " + instructorDetail.getInstructor());
+
+            //Remove the associated object reference bi-directional link
+            instructorDetail.getInstructor().setInstructorDetail(null);
+
+            System.out.println("Deleting the associated instructor");
+            session.delete(instructorDetail);
 
             session.getTransaction().commit();
         }catch (Exception e){
--- src/br/pessoal/hibernate/demo/entity/InstructorDetail.java
+++ src/br/pessoal/hibernate/demo/entity/InstructorDetail.java
@@ -24,7 +24,8 @@ public class InstructorDetail {
     @Column
     private String hobby;
 
-    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "instructorDetail", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+            CascadeType.REFRESH})
     private Instructor instructor;
 
     public InstructorDetail() {
